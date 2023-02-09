2913aae8910abed43adeb4f014b0fa6ac876ac93
Francisco Abreu <francisco.abreu@externos-cl.cencosud.com>
2023-02-09 09:44:23 -0300

    First Commit.

---
+++ src/main/java/com/example/read_mqueue/ReadMqueueApplication.java
@@ -0,0 +1,13 @@
+package com.example.read_mqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadMqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadMqueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/read_mqueue/ReadMqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.read_mqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadMqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
